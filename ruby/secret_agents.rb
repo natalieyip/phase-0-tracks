105680b42270e91050770b82392bb5123c78a27e
natalieyip <yip.natalie92@gmail.com>
2017-01-29 13:39:48 -0800

    Add reflection and driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,21 +1,48 @@
+
 def encrypt(string)
 	index = 0
+	newString = ""
 	while index < string.length 
 		letter = string[index]
-		print letter.next
+		if letter == "z"
+			newString += "a"
+		else
+			newString += letter.next
+		end
 		index += 1
 	end  
+	p newString
 end
-
+	
 
 def decrypt(string)
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	newString = ""
 	index = 0 
 	while index < string.length 
  	newIndex = alphabet.index(string[index]) - 1
- 	print alphabet[newIndex]
+ 	newString += alphabet[newIndex]
 	index += 1
 	end 
+	p newString
 end 
 
-decrypt("bcd")
+#Driver Code
+
+#asks the user the decrypt or encrypt preference 
+puts "Would you like to decrypt or encrypt your password?"
+en_or_de = gets.chomp
+
+#asks the user for their password 
+puts "What is your password?" 
+password = gets.chomp
+
+#printing their result
+if "#{en_or_de}" == "encrypt"
+	puts "Your new password is " + encrypt("#{password}")
+else 
+	puts "Your new password is " + decrypt("#{password}")
+end
+
+
+#It works because the order of evaluation causes  #swordfish to encrypt first, then decrypt.
