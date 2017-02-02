3a431352782885f04629116e1d7633b98bf49881
natalieyip <yip.natalie92@gmail.com>
2017-02-02 15:59:26 -0800

    Demonstrate how to access items in nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -23,16 +23,37 @@ bachelorette_party = {
 			"Wine",
 			"Cheese"
 		],
-		stripper: "Magic Mike",
+		stripper: "Magic Tatum",
 		activities: [
 			"mechanical bull",
 			"yoga",
 			"leap frog"
 		],
 		party_favors: { 
-			candy_bracelets: 15
+			candy_bracelets: 15,
 			yoga_mats: 3
 		}
 	}
 }
 
+bachelorette_party[:first_half][:food][1]
+#"Pecan Pies"
+
+bachelorette_party[:second_half][:party_favors][:candy_bracelets]
+#15
+
+bachelorette_party[:first_half][:stripper]
+#"Paul the Pool Boy"
+
+bachelorette_party[:first_half][:food]
+# ["Double Decker Sliders", "Pecan Pies", "Bacon Wrapped Hot Dogs", "Goat Meat"]
+
+bachelorette_party[:second_half][:party_favors][:beach_balls] = 100
+# :party_favors=>{:candy_bracelets=>15, :yoga_mats=>3, :beach_balls=>100}
+
+bachelorette_party[:second_half][:stripper].prepend("Channing ")
+# "Channing Magic Tatum"
+
+bachelorette_party[:first_half][:activities].pop
+bachelorette_party[:first_half][:activities].push("eating")
+# ["knitting", "eating"]
