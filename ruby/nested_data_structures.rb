1a4ca40f5702e0167e0e5eb6011afefb17f5bb0e
natalieyip <yip.natalie92@gmail.com>
2017-02-02 15:44:31 -0800

    Make a construct

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,38 @@
+bachelorette_party = {
+	first_half: { 
+		location: "Penthouse Suite",
+		food: [
+			"Double Decker Sliders", 
+			"Pecan Pies",
+			"Bacon Wrapped Hot Dogs", 
+			"Goat Meat"
+		],  
+		stripper: "Paul the Pool Boy", 
+		activities: [
+			"knitting",
+			"reading"
+		],
+		party_favors: { 
+			lollipops: 20,
+			knitting_needles: 5
+		}
+		}, 
+	second_half: {
+		location: "Ocean", 
+		food: [
+			"Wine",
+			"Cheese"
+		],
+		stripper: "Magic Mike",
+		activities: [
+			"mechanical bull",
+			"yoga",
+			"leap frog"
+		],
+		party_favors: { 
+			candy_bracelets: 15
+			yoga_mats: 3
+		}
+	}
+}
+
