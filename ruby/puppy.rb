7c8a3d0cf25f847090c8ebb8ad9c5a7d260df95b
natalieyip <yip.natalie92@gmail.com>
2017-02-05 16:08:54 -0800

    Design a class

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+SPECIES ------------------------
+Pupptastic Puppulus 
+
+CHARACTERISTICS ----------------
+Fur: yes 
+Color: varies 
+Friendly: yes
+Tail: 0 to none
+Eye count: 2
+Name: varies
+Hungry: yes 
+Paws: 4 
+Ears: 2
+
+
+BEHAVIOR -----------------------
+Drool
+Jump
+Bark
+Runs
+Swims
+Rolls over
