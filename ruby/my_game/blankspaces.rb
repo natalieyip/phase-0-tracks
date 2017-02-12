3baaf4c4f76262cd462d0d4fc715a8b3cb1c4de0
natalieyip <yip.natalie92@gmail.com>
2017-02-12 11:31:17 -0800

    Make new instance methods

--- ruby/my_game/blankspaces.rb
+++ ruby/my_game/blankspaces.rb
@@ -1,79 +1,59 @@
 
 class Blankspaces 
 
-  attr_reader 
-  attr_accessor :secret_word, :guess_count
+  attr_accessor :secret_word, :guess_count, :word_being_guess
 	
   def initialize(secret_word)
     @secret_word = secret_word
     @guess_count = 0
+    @word_being_guess = []
   end
 
-=begin
+
   def blanks 
-    word_being_guess = []
     secret_word.length.times { word_being_guess << "_ "}
     word_being_guess.join
   end 
 
 
-  def guess_letter(letter)
+  def guess_right_letter(letter)
     if @secret_word.index(letter) != nil #got something right 
-       @word_being_guess.delete_at(secret_word.index(letter))
-       word_being_guess.insert(secret_word.index(letter), letter)
-       word_being_guess.join
-     else 
-       guess_count += 1
+       @word_being_guess.delete_at(@secret_word.index(letter))
+       @word_being_guess.insert(@secret_word.index(letter), letter)
+       p @word_being_guess.join
     end
-  end
-
-  def last_message
   end 
-=end 
+
+  def guess_wrong_letter(letter)
+    @guess_count += 1 
+  end 
 
 end
 
 
 #user interface 
-p "User 1: Please enter secret word: "
+puts "User 1: Please enter secret word: "
 secret_word = gets.chomp 
 game = Blankspaces.new(secret_word)
 
-word_being_guess = []
+p game.blanks 
 
-secret_word.length.times { word_being_guess << "_ "}
-
-p "User 2: Please start guessing the word."
+puts "User 2: Please start guessing the word."
   
 while game.guess_count < game.secret_word.length 
   user_guess = gets.chomp 
-  
-  if secret_word.index(user_guess) != nil #got something right 
-    
-     correct_guess = secret_word.index(user_guess) #this prints out number 
-     word_being_guess.delete_at(correct_guess)
-     word_being_guess.insert(correct_guess, user_guess)
-     correct_so_far = word_being_guess.join
-     puts correct_so_far
-     
-     break if correct_so_far == secret_word
-     
-     puts "Great, you got one! Guess again"
-    
-   else 
-     
-     puts "Nope try again"
-     game.guess_count += 1
+
+  if game.guess_right_letter(user_guess)
+    break if game.word_being_guess.join == secret_word
+    puts "Great, you got one! Guess again."
+  elsif game.guess_wrong_letter(user_guess)
+    puts "Sorry try again."
   end 
-  
-  
 end 
 
-if correct_so_far == secret_word
+if game.word_being_guess.join  == secret_word
   puts "What a smartie!"
 else 
   puts "Darn, the word was #{secret_word}!"
 end
 
-
-#having trouble converting my program into the class. 
