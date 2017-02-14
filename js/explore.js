2aae7348ec1d3e1197bbb81a39a768e21b2a90e8
natalieyip <yip.natalie92@gmail.com>
2017-02-13 18:49:15 -0800

    Make a reverse function for strings

--- js/explore.js
+++ js/explore.js
@@ -1,11 +1,20 @@
 /*
 PSEUDOCODE 
 
-- Make a new variable and set it to an empty string 
-- Iterate through each letter of a string
+- Make a new variable and set it to an empty array 
+- Iterate through each letter of the string argument
 - With a for loop, we can increment counting down starting from the last letter
-- Push or add each new letter to the empty string variable 
-- Return the variable 
+- Push or add each new letter to the empty array  
+- Return the variable and used a join method to return the string
 
 */
 
+function reverse(string) { 
+	var reverseString = [];
+	  for (var i = string.length; i >= 0; i--) {
+  	  reverseString.push(string[i]);
+  }
+   return reverseString.join("");
+} 
+
+console.log(reverse("hello"))
