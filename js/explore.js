05e2151e4dd4c0f464995ef5609bbfebaf7f36b4
natalieyip <yip.natalie92@gmail.com>
2017-02-13 18:37:24 -0800

    Pseudocode JS file

---
+++ js/explore.js
@@ -0,0 +1,11 @@
+/*
+PSEUDOCODE 
+
+- Make a new variable and set it to an empty string 
+- Iterate through each letter of a string
+- With a for loop, we can increment counting down starting from the last letter
+- Push or add each new letter to the empty string variable 
+- Return the variable 
+
+*/
+
