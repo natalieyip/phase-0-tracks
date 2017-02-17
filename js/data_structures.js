43e69c28c0e9e3023d3cd41e74c5d9908b78c510
natalieyip <yip.natalie92@gmail.com>
2017-02-16 22:31:15 -0800

    Make two arrays

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+var colors = ["blue", "purple", "pink", "gold"];
+var names = ["Leslie Knope", "April Ludgate", "Ron Swanson", "Lil' Sebastian"];
+
+colors.push("forest green");
+names.push("Andy Dwyer"); 
+
