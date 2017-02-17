202a1cc59f6a4f8fedd7d98a8586dc775bdfbde7
natalieyip <yip.natalie92@gmail.com>
2017-02-16 22:57:33 -0800

    Make car constructor

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,21 +1,39 @@
-var colors = ["blue", "purple", "pink", "gold"];
-var names = ["Leslie Knope", "April Ludgate", "Ron Swanson", "Lil Sebastian"];
+// var colors = ["blue", "purple", "pink", "gold"];
+// var names = ["Leslie Knope", "April Ludgate", "Ron Swanson", "Lil Sebastian"];
 
-colors.push("forest green");
-names.push("Andy Dwyer"); 
+// colors.push("forest green");
+// names.push("Andy Dwyer"); 
 
-//console.log(colors);
-//console.log(names);
+// //console.log(colors);
+// //console.log(names);
 
-function assignColorToName(arr, arr2){ 
-	var parksAndRec = {};
+// function assignColorToName(arr, arr2){ 
+// 	var parksAndRec = {};
 
-	if (arr.length == arr2.length){ 
-		for (var i = 0; i < arr.length; i++){ 
-			parksAndRec[arr[i]] = arr2[i];
-		}
-	}
-	return parksAndRec;
+// 	if (arr.length == arr2.length){ 
+// 		for (var i = 0; i < arr.length; i++){ 
+// 			parksAndRec[arr[i]] = arr2[i];
+// 		}
+// 	}
+// 	return parksAndRec;
+// }
+
+// console.log(assignColorToName(names, colors));
+
+function carConstructor(model, make, year) {
+	this.model = model; 
+	this.make = make; 
+	this.year = year; 
+
+	this.turnOnRadio = function() {console.log("..Turning on radio..");};
+
 }
 
-console.log(assignColorToName(names, colors));
+var carOne = new carConstructor("Honda", "Civic", 2013);
+
+carOne.turnOnRadio();
+console.log(carOne)
+
+var carTwo = new carConstructor("Lamborgini", "Tesla", 2030);
+carTwo.turnOnRadio(); 
+console.log(carTwo)
