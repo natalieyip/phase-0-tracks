0582e875a9fc21cb9d2c66aca0758454f62dd3d3
natalieyip <yip.natalie92@gmail.com>
2017-02-16 22:40:40 -0800

    Make method for assigning colors to horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,21 @@
 var colors = ["blue", "purple", "pink", "gold"];
-var names = ["Leslie Knope", "April Ludgate", "Ron Swanson", "Lil' Sebastian"];
+var names = ["Leslie Knope", "April Ludgate", "Ron Swanson", "Lil Sebastian"];
 
 colors.push("forest green");
 names.push("Andy Dwyer"); 
 
+//console.log(colors);
+//console.log(names);
+
+function assignColorToName(arr, arr2){ 
+	var parksAndRec = {};
+
+	if (arr.length == arr2.length){ 
+		for (var i = 0; i < arr.length; i++){ 
+			parksAndRec[arr[i]] = arr2[i];
+		}
+	}
+	return parksAndRec;
+}
+
+console.log(assignColorToName(names, colors));
